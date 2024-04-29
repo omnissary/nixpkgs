220c600d896a1a34bd186c8111b5f7eabb292923
Dmitry Moskowski <me@corpix.ru>
2024-04-29 14:57:44 +0000

    gitea-actions-runner: do not escape service name
    this make number identifiers look ugly and non practical

--- nixos/modules/services/continuous-integration/gitea-actions-runner.nix
+++ nixos/modules/services/continuous-integration/gitea-actions-runner.nix
@@ -2,7 +2,6 @@
   config,
   lib,
   pkgs,
-  utils,
   ...
 }:
 
@@ -25,10 +24,6 @@ let
     mkIf
     nameValuePair
     types
-    ;
-
-  inherit (utils)
-    escapeSystemdPath
     ;
 
   cfg = config.services.gitea-actions-runner;
@@ -192,7 +187,7 @@ in
             wantsPodman = wantsContainerRuntime && config.virtualisation.podman.enable;
             configFile = settingsFormat.generate "config.yaml" instance.settings;
           in
-          nameValuePair "gitea-runner-${escapeSystemdPath name}" {
+          nameValuePair "gitea-runner-${name}" {
             inherit (instance) enable;
             description = "Gitea Actions Runner";
             wants = [ "network-online.target" ];
