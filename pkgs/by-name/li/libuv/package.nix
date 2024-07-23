d15c6c84cd666e3d55e958fef35e1133be41cdde
Dmitry Moskowski <me@corpix.ru>
2024-07-23 05:44:29 +0000

    libuv: disable tests for armv7l

--- pkgs/by-name/li/libuv/package.nix
+++ pkgs/by-name/li/libuv/package.nix
@@ -176,7 +176,8 @@ stdenv.mkDerivation (finalAttrs: {
 
   doCheck =
     # routinely hangs on powerpc64le
-    !stdenv.hostPlatform.isPower64;
+    !stdenv.hostPlatform.isPower64
+    && !stdenv.hostPlatform.isArmv7;
 
   # Tests like to time out when run in parallel to large builds
   preCheck = ''
